13be08c9e6c283acd7f660104d633f00abb7546f
Kieran Goodary <kieran@digitalindustria.com>
2020-03-23 16:11:10 +0000

    Abstracted the HUD Drawer to it's own React component.

--- app/src/components/GlobalMap.js
+++ app/src/components/GlobalMap.js
@@ -2,11 +2,7 @@ import React from 'react';
 import { GoogleMap, LoadScript } from '@react-google-maps/api'
 import { Container } from '@material-ui/core';
 
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
+import HudDrawer from '../components/HudDrawer';
 
 export default class GlobalMap extends React.Component {
   constructor(props) {
@@ -21,35 +17,8 @@ export default class GlobalMap extends React.Component {
         id="global-map-container"
         maxWidth={false}>
         <Container id="hud" maxWidth="sm">
-          <Card className="card-pane">
-            <CardContent>
-              <Typography variant="h5" component="h1" gutterBottom>
-                Ground Truth Network
-              </Typography>
-              <Typography component="p">
-                Ground Truth gives everyone the opportunity to report their symptoms anonymously for the benefit of others.
-              </Typography>
-              <Typography variant="body2" component="p">
-                
-              </Typography>
-            </CardContent>
-            <CardActions>
-              <Button size="small">Report</Button>
-            </CardActions>
-          </Card>
-
-          <Card className="card-pane">
-            <CardContent>
-              <Typography variant="h5" component="p">
-                Connected.
-              </Typography>
-              <Typography component="p">
-                You're anonymously connected to our network.
-              </Typography>
-            </CardContent>
-          </Card>
+          <HudDrawer />
         </Container>
-
 
         <LoadScript
           id="script-loader"
---
+++ app/src/components/HudDrawer.js
@@ -0,0 +1,57 @@
+import React from 'react';
+
+import ExpansionPanel from '@material-ui/core/ExpansionPanel';
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import Typography from '@material-ui/core/Typography';
+
+export default class HudDrawer extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <ExpansionPanel>
+          <ExpansionPanelSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel1a-content"
+            id="panel1a-header">
+            <Typography>Ground Truth Network</Typography>
+          </ExpansionPanelSummary>
+          <ExpansionPanelDetails>
+            <Typography>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
+              sit amet blandit leo lobortis eget.
+            </Typography>
+          </ExpansionPanelDetails>
+        </ExpansionPanel>
+
+        <ExpansionPanel>
+          <ExpansionPanelSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel3a-content"
+            id="panel3a-header">
+            <Typography>
+              <b>Report Now</b>
+            </Typography>
+          </ExpansionPanelSummary>
+        </ExpansionPanel>
+
+        <ExpansionPanel>
+          <ExpansionPanelSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel2a-content"
+            id="panel2a-header"
+          >
+            <Typography>Statistics</Typography>
+          </ExpansionPanelSummary>
+          <ExpansionPanelDetails>
+            <Typography>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
+              sit amet blandit leo lobortis eget.
+            </Typography>
+          </ExpansionPanelDetails>
+        </ExpansionPanel>
+      </React.Fragment>
+    )
+  }
+}
