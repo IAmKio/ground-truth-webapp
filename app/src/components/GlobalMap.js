af94a252c47d23a02aaecca28fff378bd8a216c3
Kieran Goodary <kieran@digitalindustria.com>
2020-03-23 21:38:26 +0000

    Rough heatmap element added.

--- app/src/components/GlobalMap.js
+++ app/src/components/GlobalMap.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { GoogleMap, LoadScript } from '@react-google-maps/api'
+import { GoogleMap, LoadScript, HeatmapLayer } from '@react-google-maps/api'
 import { Container } from '@material-ui/core';
 
 import HudDrawer from '../components/HudDrawer';
@@ -7,14 +7,54 @@ import HudDrawer from '../components/HudDrawer';
 export default class GlobalMap extends React.Component {
   constructor(props) {
     super(props);
+
+    this.googleMapLibraries = ['visualization']
+    this.state = {
+      isReady: false,
+      markers: [],
+      GoogleLatLngFn: null,
+      map: null,
+    }
   }
 
   componentDidUpdate() {
     console.log('Component GlobalMap updated:', this.props.loaded);
+    console.log(this.props);
+
+    if (this.props.hotspots.length > 0 && this.state.isReady) {
+      this.state.markers = [];
+      this.props.hotspots.forEach(hotspot => {
+        const position = {
+          lat: hotspot.geopoint._latitude,
+          lng: hotspot.geopoint._longitude,
+        }
+        const thisMarker = new this.state.GoogleLatLngFn(position.lat, position.lng);
+        this.state.markers.push(thisMarker);
+      });
+    }
+
+    console.log(this.state.markers);
   }
 
   componentDidMount() {
     console.log('Component GlobalMap mounted.');
+
+    setInterval(() => {
+      console.log('Refreshing ...');
+      this.setState({});
+    }, 20000)
+  }
+
+  onLoad() {
+    console.log('GM onLoad');
+  }
+
+  onUnmount() {
+    console.log('GM onUnmount');
+  }
+
+  onDragEnd() {
+    console.log('Drag end.');
   }
 
   render() {
@@ -29,14 +69,28 @@ export default class GlobalMap extends React.Component {
         <LoadScript
           id="script-loader"
           googleMapsApiKey="AIzaSyDW_dP3NjuXPjvBs0AXKOL2BuQ4susF3KU"
-        >
-          <GoogleMap
-            id='global-map'
-            zoom={7}
-            center={{
-              lat: -3.745,
-              lng: -38.523
-            }}>
+          libraries={this.googleMapLibraries}>
+            <GoogleMap
+              onDragEnd={this.onDragEnd}
+              onLoad={map => {
+                this.setState({
+                  isReady: true,
+                  GoogleLatLngFn: window.google.maps.LatLng,
+                  map: map,
+                });
+              }}
+              id='global-map'
+              zoom={7}
+              center={{
+                lat: 51.3360,
+                lng: 0.2674
+              }}>
+
+            <HeatmapLayer
+              onLoad={this.onLoad}
+              onUnmount={this.onUnmount}
+              data={this.state.markers}
+            />
           </GoogleMap>
         </LoadScript>
       </Container>
--- app/src/screens/home/Home.js
+++ app/src/screens/home/Home.js
@@ -7,23 +7,40 @@ export default class HomeScreen extends React.Component {
     super(props);
 
     this.state = {
-      loaded: false
+      loaded: false,
+      announcements: [],
+      hotspots: [],
+      layers: []
     };
   }
 
-  componentDidMount() {
+  async componentDidMount() {
     console.log('Home component mounted.');
+
+    const announcementsResponse = await fetch('https://api.groundtruth.app/announcements', {});
+    const announcementsJson = await announcementsResponse.json();
+
+    const hotspotsResponse = await fetch('https://api.groundtruth.app/hotspots', {});
+    const hotspotsJson = await hotspotsResponse.json();
+
+    console.log('Hotspots JSON', hotspotsJson);
+
 
     setTimeout(() => {
       this.setState({
-        loaded: true
+        loaded: true,
+        announcements: announcementsJson.announcements,
+        hotspots: hotspotsJson.hotspots
       });
     }, 1000);
   }
 
   render() {
     return <div>
-      <GlobalMap loaded={this.state.loaded} />
+      <GlobalMap
+        loaded={this.state.loaded}
+        hotspots={this.state.hotspots}
+        announcements={this.state.announcements} />
     </div>
   }
 }
