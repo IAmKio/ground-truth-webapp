de834d4710428cdd4bbc239f7ef2c52a53af8d2b
Kieran Goodary <kieran@digitalindustria.com>
2020-03-26 15:16:08 +0000

    Added licenses and font theme to components.

--- app/src/components/HudDrawer.js
+++ app/src/components/HudDrawer.js
@@ -1,3 +1,17 @@
+// Copyright 2020 Kieran Goodary, Digital Industria Ltd
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 import React from 'react';
 
 import Grid from '@material-ui/core/Grid';
@@ -75,7 +89,9 @@ export default class HudDrawer extends React.Component {
                 <Icon>my_location</Icon>
               </Grid>
               <Grid item>
-                <b>&nbsp;&nbsp;REPORT NOW</b>
+                <Typography>
+                  <b>&nbsp;&nbsp;Report Now</b>
+                </Typography>
               </Grid>
             </Grid>
           </ExpansionPanelSummary>
--- app/src/screens/home/Home.js
+++ app/src/screens/home/Home.js
@@ -1,4 +1,20 @@
+// Copyright 2020 Kieran Goodary, Digital Industria Ltd
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 import React from 'react';
+
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 
 import GlobalMap from '../../components/GlobalMap';
 
@@ -44,13 +60,28 @@ export default class HomeScreen extends React.Component {
   }
 
   render() {
+    const theme = createMuiTheme({
+      typography: {
+        fontFamily: 'Roboto Mono, sans-serif',
+      },
+      overrides: {
+        MuiCssBaseline: {
+          '@global': {
+            '@font-face': 'Roboto Mono',
+          },
+        },
+      },
+    });
+
     return <div>
-      <GlobalMap
-        loaded={this.state.loaded}
-        hotspots={this.state.hotspots}
-        announcements={this.state.announcements}
-        statistics={this.state.statistics}
-        layers={this.state.layers} />
+      <ThemeProvider theme={theme}>
+        <GlobalMap
+          loaded={this.state.loaded}
+          hotspots={this.state.hotspots}
+          announcements={this.state.announcements}
+          statistics={this.state.statistics}
+          layers={this.state.layers} />
+        </ThemeProvider>
     </div>
   }
 }
